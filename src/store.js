296d6a7a9cd49be64a3f5592a125e93ca7974e51
Jesus Amaya <jesus.amaya@rulesware.com>
2017-10-18 23:37:55 -0600

    cleaning up things a bit

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose  } from 'redux'
 import thunk from 'redux-thunk'
-import logger from 'redux-logger'
+// import logger from 'redux-logger'
 import rootReducer from 'containers/App/reducer'
 import { routerMiddleware } from 'react-router-redux';
 
@@ -9,17 +9,13 @@ export default function configureStore( history ){
 
   const routerMiddleWare= routerMiddleware(history)
 
-  const middlewares= [thunk, routerMiddleWare, logger]
+  const middlewares= [thunk, routerMiddleWare/*, logger*/]
   const enhancers = [applyMiddleware(...middlewares)]
 
-  // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const composeEnhancers =
   typeof window === 'object' &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
       ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // TODO Try to remove when `react-router-redux` is out of beta,
-        //LOCATION_CHANGE should not be fired more than once after hot reloading
-        // Prevent recomputing reducers for `replaceReducer`
         shouldHotReload: false,
       })
       : compose;
